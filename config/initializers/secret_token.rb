e43af66435bea106edf9a0f3d517f07bf2644255
Chris Reid <chris.reid@suite101.com>
2011-02-26 21:56:36 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geekmatchr::Application.config.secret_token = '8e1bb66e04d15d0b23e82e3b85f63f6b985044646aecfa4e6d24218aec03137bafefa9d9eccab6e2c132342dba718f831a297b279618d2c81e240529938ff075'
