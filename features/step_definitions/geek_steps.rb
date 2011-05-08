fb91136fe5454e346c79f20d7aa5c32cecb5a330
Chris Reid <chris.reid@suite101.com>
2011-05-08 11:19:21 -0700

    adds profile step definitions

--- features/step_definitions/geek_steps.rb
+++
@@ -1,3 +0,0 @@
-Given /^a geek with "([^"]*)" profiles$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
---
+++ features/step_definitions/profile_steps.rb
@@ -0,0 +1,12 @@
+Given /^I have "([^"]*)" profiles$/ do
+  |profile_count|
+#  pending # express the regexp above with the code you wish you had
+end
+
+When /^I post a profile$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see that I have "([^"]*)" profile$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
