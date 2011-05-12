c9c03e9312602f63ad781f3ad00ea31476e1f0af
Chris Reid <chris.reid@suite101.com>
2011-05-12 15:10:34 -0700

    enables mocha so tests pass

--- features/profile.feature
+++ features/profile.feature
@@ -6,9 +6,9 @@ Feature: Profile
 
 Scenario: Successful submission
   Given that I am logged in
-  When I go to the profile page
+  When I go to the edit current user page
   And I fill in "user_description" with "I'm a rails developer who is awesome."
   And I fill in "user_skills" with "rails devops cucumber"
   And I fill in "user_ideas" with "real_estate cloud_manufacturing"
-  And I press "Save"
-  Then I should see "Thank you. Your profile has been saved successfully."
+  And I press "Update"
+  Then I should see "Your profile has been updated."
--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -1,12 +1,16 @@
-Given /^I have "([^"]*)" profiles$/ do
-  |profile_count|
-#  pending # express the regexp above with the code you wish you had
+Given /^that I am logged in$/ do
+  @current_user = User.create!(
+   :username => 'test',
+   :email => 'test@raygun6.com',
+   :password => 'test',
+   :password_salt => '356a192b7913b04c54574d18c28d46e6395428ab',
+   :password_hash => 'df42adbd0b4f7d31af495bcd170d4496686aecb1',
+   :created_at => '2008-11-22 12:13:59')
+
+  visit("/login")
+  fill_in("Username or Email Address", :with => "test")
+  fill_in("password", :with => "test")
+  click_button("Log in")
 end
 
-When /^I post a profile$/ do
-  pending # express the regexp above with the code you wish you had
-end
 
-Then /^I should see that I have "([^"]*)" profile$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
