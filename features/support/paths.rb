b291f4598db685dc1a218f2af6ca4d2c1e282d81
Chris Reid <chris@raygun6.com>
2011-05-27 21:48:29 -0700

    updates scenario paths

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,6 +17,9 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
+    when /^(.*)'s profile page$/i
+      user_profile_path(User.find_by_login($1))
+
     else
       begin
         page_name =~ /the (.*) page/
