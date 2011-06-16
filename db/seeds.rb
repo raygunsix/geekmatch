a970969f653e271a7221f5d5ebb124b116046685
Chris Reid <chris@raygun6.com>
2011-06-15 22:20:20 -0700

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+#users = []
+#users <<
+User.create!(:username => 'chris',
+             :email => 'chris@raygun6.com',
+             :password => 'ratboy6',
+             :description => 'i am cool',
+             :skills => 'magic php mysql',
+             :ideas => 'airships light_sabers'
+            )
+
+User.create!(:username => 'rico',
+             :email => 'rico@hotmail.com',
+             :password => 'southpark',
+             :description => 'the raddest dude ever',
+             :skills => 'jquery vms poker',
+             :ideas => 'nuclear_windmills light_sabers'
+            )
+            
+User.create!(:username => 'bob',
+             :email => 'bob334@yahoo.com',
+             :password => 'canucks',
+             :description => 'Interested in starting an online poker website',
+             :skills => 'java rails poker',
+             :ideas => 'online_poker virtual_truck_stop'
+            )
+
+User.create!(:username => 'larry',
+             :email => 'lrry@gmail.com',
+             :password => 'rabbit',
+             :description => 'level 88 blood elf hunter',
+             :skills => 'prototype postgresql php',
+             :ideas => 'real_estate_website light_sabers'
+            )
