dcddf27d9f76306e5a7643021fa57ec919d4ff4f
Chris Reid <chris@raygun6.com>
2011-08-02 21:43:11 -0700

    updates startup code to use factories instead of fixtures

---
+++ spec/factories/startups.rb
@@ -0,0 +1,7 @@
+Factory.define :startup do |startup|
+  startup.title 'fooco'
+  startup.city 'foocity'
+  startup.description 'Discount real estate website' 
+  startup.skills 'rails'
+  startup.user_id 1
+end
